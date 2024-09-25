8284e029c8e0acba8751554e3964351bd282d019
JDaza13 <jdaza1327@gmail.com>
2024-09-24 21:03:19 -0500

    ci: outputs deployed site url

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -9,5 +9,6 @@ import exec from "@actions/exec";
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`);
 
-  core.notice("Hello from custom JS Action!");
+  const websiteUrl = `http://${bucket}.s3-website.${region}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 })();
