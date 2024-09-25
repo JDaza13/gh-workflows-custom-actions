35edcbad126cfaebb2123fa4c7236f21ce316924
JDaza13 <jdaza1327@gmail.com>
2024-09-24 20:52:18 -0500

    ci: custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,7 +1,13 @@
 import core from "@actions/core";
-import github from "@actions/github";
-import exex from "@actions/exec";
+import exec from "@actions/exec";
 
 (() => {
+  const bucket = core.getInput("bucket", { required: true });
+  const region = core.getInput("region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  const s3Uri = `s3;//${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`);
+
   core.notice("Hello from custom JS Action!");
 })();
